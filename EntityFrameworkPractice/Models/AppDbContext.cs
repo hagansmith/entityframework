4db8a516427baa5824839a528abff8679b0b1a38
Adam Smith <hagansmith@gmail.com>
2018-05-08 20:04:44 -0500

    setup project with entity framework and create initial migrations

---
+++ EntityFrameworkPractice/Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameworkPractice.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("EntityFramework") { }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ EntityFrameworkPractice/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameworkPractice.Models
+{
+    public class Order
+    {
+        public virtual int Id { get; set; }
+        public virtual int Total { get; set; }
+    }
+}
